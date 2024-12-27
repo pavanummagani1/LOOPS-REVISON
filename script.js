16da4ebf9d2bb9a556b93929a1009ac07942ef2a
PavanUmmagani07 <pavankalyanU@outlook.com>
2024-12-27 21:45:48 +0530

    LOOPS REVISION

---
+++ script.js
@@ -0,0 +1,31 @@
+// WHILE LOOP
+/**
+ * SYNTAX:
+ * Decalaration
+ * while(condition){
+ * //block of code
+ * Updation
+ * }
+ * 
+ */
+
+// Print the first 10 natural numbers
+let i =1;
+while(i<=10){
+    console.log(i)
+    i++;
+}
+
+// for LOOP
+
+/**
+ * SYNTAX:
+ * for(initialisation; condition; updation){
+ *  //block of code
+ * }
+ */
+
+//// Print the first 10 natural numbers
+for(let i =1; i<=10;i++){
+    console.log(i);
+}
